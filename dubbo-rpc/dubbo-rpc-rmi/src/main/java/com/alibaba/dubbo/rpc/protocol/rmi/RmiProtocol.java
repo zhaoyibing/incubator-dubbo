e058f1b973f3cdc5deb2e2ab8ad1968841576c82
crazyhzm <crazyhzm@gmail.com>
2019-01-29 11:22:59 +0800

    【源码分析】dubbo源码解析（三十一）远程调用——rmi协议

--- dubbo-rpc/dubbo-rpc-rmi/src/main/java/com/alibaba/dubbo/rpc/protocol/rmi/RmiProtocol.java
+++ dubbo-rpc/dubbo-rpc-rmi/src/main/java/com/alibaba/dubbo/rpc/protocol/rmi/RmiProtocol.java
@@ -38,6 +38,9 @@ import java.rmi.RemoteException;
  */
 public class RmiProtocol extends AbstractProxyProtocol {
 
+    /**
+     * 默认端口
+     */
     public static final int DEFAULT_PORT = 1099;
 
     public RmiProtocol() {
@@ -49,14 +52,29 @@ public class RmiProtocol extends AbstractProxyProtocol {
         return DEFAULT_PORT;
     }
 
+    /**
+     * 服务暴露
+     * @param impl
+     * @param type
+     * @param url
+     * @param <T>
+     * @return
+     * @throws RpcException
+     */
     @Override
     protected <T> Runnable doExport(final T impl, Class<T> type, URL url) throws RpcException {
+        // rmi暴露者
         final RmiServiceExporter rmiServiceExporter = new RmiServiceExporter();
+        // 设置端口
         rmiServiceExporter.setRegistryPort(url.getPort());
+        // 设置服务名称
         rmiServiceExporter.setServiceName(url.getPath());
+        // 设置接口
         rmiServiceExporter.setServiceInterface(type);
+        // 设置服务实现
         rmiServiceExporter.setService(impl);
         try {
+            // 初始化bean的时候执行
             rmiServiceExporter.afterPropertiesSet();
         } catch (RemoteException e) {
             throw new RpcException(e.getMessage(), e);
@@ -65,6 +83,7 @@ public class RmiProtocol extends AbstractProxyProtocol {
             @Override
             public void run() {
                 try {
+                    // 销毁
                     rmiServiceExporter.destroy();
                 } catch (Throwable e) {
                     logger.warn(e.getMessage(), e);
@@ -73,25 +92,43 @@ public class RmiProtocol extends AbstractProxyProtocol {
         };
     }
 
+    /**
+     * 服务引用
+     * @param serviceType
+     * @param url
+     * @param <T>
+     * @return
+     * @throws RpcException
+     */
     @Override
     @SuppressWarnings("unchecked")
     protected <T> T doRefer(final Class<T> serviceType, final URL url) throws RpcException {
+        // FactoryBean对于RMI代理，支持传统的RMI服务和RMI调用者，创建RmiProxyFactoryBean对象
         final RmiProxyFactoryBean rmiProxyFactoryBean = new RmiProxyFactoryBean();
         // RMI needs extra parameter since it uses customized remote invocation object
+        // 检测版本
         if (url.getParameter(Constants.DUBBO_VERSION_KEY, Version.getProtocolVersion()).equals(Version.getProtocolVersion())) {
             // Check dubbo version on provider, this feature only support
+            // 设置RemoteInvocationFactory以用于此访问器
             rmiProxyFactoryBean.setRemoteInvocationFactory(new RemoteInvocationFactory() {
                 @Override
                 public RemoteInvocation createRemoteInvocation(MethodInvocation methodInvocation) {
+                    // 自定义调用工厂可以向调用添加更多上下文信息
                     return new RmiRemoteInvocation(methodInvocation);
                 }
             });
         }
+        // 设置此远程访问者的目标服务的URL。URL必须与特定远程处理提供程序的规则兼容。
         rmiProxyFactoryBean.setServiceUrl(url.toIdentityString());
+        // 设置要访问的服务的接口。界面必须适合特定的服务和远程处理策略
         rmiProxyFactoryBean.setServiceInterface(serviceType);
+        // 设置是否在找到RMI存根后缓存它
         rmiProxyFactoryBean.setCacheStub(true);
+        // 设置是否在启动时查找RMI存根
         rmiProxyFactoryBean.setLookupStubOnStartup(true);
+        // 设置是否在连接失败时刷新RMI存根
         rmiProxyFactoryBean.setRefreshStubOnConnectFailure(true);
+        // // 初始化bean的时候执行
         rmiProxyFactoryBean.afterPropertiesSet();
         return (T) rmiProxyFactoryBean.getObject();
     }
@@ -104,10 +141,13 @@ public class RmiProtocol extends AbstractProxyProtocol {
         if (e != null && e.getCause() != null) {
             Class<?> cls = e.getCause().getClass();
             if (SocketTimeoutException.class.equals(cls)) {
+                // 抛出超时时间
                 return RpcException.TIMEOUT_EXCEPTION;
             } else if (IOException.class.isAssignableFrom(cls)) {
+                // 抛出网络异常
                 return RpcException.NETWORK_EXCEPTION;
             } else if (ClassNotFoundException.class.isAssignableFrom(cls)) {
+                // 抛出序列化异常
                 return RpcException.SERIALIZATION_EXCEPTION;
             }
         }
--- dubbo-rpc/dubbo-rpc-rmi/src/main/java/com/alibaba/dubbo/rpc/protocol/rmi/RmiRemoteInvocation.java
+++ dubbo-rpc/dubbo-rpc-rmi/src/main/java/com/alibaba/dubbo/rpc/protocol/rmi/RmiRemoteInvocation.java
@@ -33,23 +33,29 @@ public class RmiRemoteInvocation extends RemoteInvocation {
      */
     public RmiRemoteInvocation(MethodInvocation methodInvocation) {
         super(methodInvocation);
+        // 添加dubbo附加值的属性
         addAttribute(dubboAttachmentsAttrName, new HashMap<String, String>(RpcContext.getContext().getAttachments()));
     }
 
     /**
      * Need to restore context on provider side (Though context will be overridden by Invocation's attachment
      * when ContextFilter gets executed, we will restore the attachment when Invocation is constructed, check more
+     * 需要在提供者端恢复上下文（尽管上下文将被Invocation的附件覆盖
+     * 当ContextFilter执行时，我们将在构造Invocation时恢复附件，检查更多
      * from {@link com.alibaba.dubbo.rpc.proxy.InvokerInvocationHandler}
      */
     @SuppressWarnings("unchecked")
     @Override
     public Object invoke(Object targetObject) throws NoSuchMethodException, IllegalAccessException,
             InvocationTargetException {
+        // 获得上下文
         RpcContext context = RpcContext.getContext();
+        // 设置参数
         context.setAttachments((Map<String, String>) getAttribute(dubboAttachmentsAttrName));
         try {
             return super.invoke(targetObject);
         } finally {
+            // 清空参数
             context.setAttachments(null);
         }
     }
